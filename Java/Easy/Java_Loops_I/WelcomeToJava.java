02a454aa9e26b080c139c428bf93fbbe9c2b4e5b
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-10-31 22:09:05 -0500

    1- Welcome to Java

---
+++ Java/Easy/Java_Loops_I/WelcomeToJava.java
@@ -0,0 +1,21 @@
+/*
+Welcome to the world of Java! In this challenge, we practice printing to stdout.
+Input Format
+
+There is no input for this challenge.
+
+Output Format
+
+You must print two lines of output:
+	Print Hello, World. on the first line.
+	Print Hello, Java. on the second line.
+*/
+
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
