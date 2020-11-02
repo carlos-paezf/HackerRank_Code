9ff4dc83834926c87226f15ef531cd854c211bef
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-11-01 19:47:35 -0500

    4- Java Stdin and Stdout II

---
+++ Java/Easy/Java_Loops_I/Java_Stdin_and_Stdout_II.java
@@ -0,0 +1,36 @@
+/**
+ * Input Format
+ *  There are three lines of input:
+ *      1. The first line contains an integer. 
+ *      2. The second line contains a double. 
+ *      3. The third line contains a String. 
+ * Output Format
+ *  There are three lines of output:
+ *      1. On the first line, print String: followed by the unaltered String read from stdin. 
+ *      2. On the second line, print Double: followed by the unaltered double read from stdin. 
+ *      3. On the third line, print Int: followed by the unaltered integer read from stdin. 
+ * 
+ * Note: If you use the nextLine() method immediately following the nextInt()
+ * method, recall that nextInt() reads integer tokens; because of this, the last
+ * newline character for that line of integer input is still queued in the input
+ * buffer and the next nextLine() will be reading the remainder of the integer
+ * line (which is empty).
+ */
+
+import java.util.Scanner;
+
+public class Java_Stdin_and_Stdout_II {
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
